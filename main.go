88d36f1f2f28240c3927f33d6b3ec29363604d17
Sean Velasco <seanvelasco@ieee.org>
2022-05-26 11:26:44 +0000

    Moved functions to handlers

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func settings(w http.ResponseWriter, req *http.Request) {
 
 }
 
-func connectivity(w http.ResponseWriter, req *http.Request) {
+func getConnectivity(w http.ResponseWriter, req *http.Request) {
 	connectivity, err := networkmanager.CheckConnectivity()
 	if err != nil {
 		panic(err)
@@ -35,7 +35,7 @@ func connectivity(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "%d", connectivity)
 }
 
-func devices(w http.ResponseWriter, req *http.Request) {
+func getAllDevices(w http.ResponseWriter, req *http.Request) {
 	devices, err := networkmanager.ListDevices()
 	if err != nil {
 		panic(err)
@@ -149,28 +149,75 @@ func addNetwork(w http.ResponseWriter, req *http.Request) {
 // 	fmt.Fprintf(w, "%s", jsonString)
 // }
 
-func main() {
-
-	// wirelessDevices, _ := networkmanager.GetDeviceByType(2)
-	// wirelessDevice := wirelessDevices[0].(map[string]interface{})["DevicePath"].(dbus.ObjectPath)
-
-	// wirelessNetwork := networkmanager.APNetwork{
-	// 	Ssid:     "Test",
-	// 	Password: "Test",
-	// 	Device:   wirelessDevice,
-	// }
-
-	// networkmanager.CreateAccessPoint(wirelessNetwork)
-
-	//////////////////////////////
-
+func internetSharing() {
 	wiredDevices, _ := networkmanager.GetDeviceByType(1)
 
 	fmt.Println(wiredDevices)
 
+	// Get first wired device (eth0)
+
 	wiredDevice := wiredDevices[0].(map[string]interface{})["DevicePath"].(dbus.ObjectPath)
 
 	networkmanager.InternetSharingOverEthernet(wiredDevice)
+}
+
+func scanWirelessNetwork(w http.ResponseWriter, req *http.Request) {
+	wirelessDevices, _ := networkmanager.GetDeviceByType(2)
+	// Get first wireless device (wlan0)
+	wirelessDevice := wirelessDevices[0].(map[string]interface{})["DevicePath"].(dbus.ObjectPath)
+	wirelessNetworks, err := networkmanager.GetAccessPoints(wirelessDevice)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		ResponseMessage.Success = false
+		ResponseMessage.Message = "Failed to create access point"
+		jsonString, _ := json.Marshal(ResponseMessage)
+		fmt.Fprintf(w, "%s", jsonString)
+		return
+	}
+	jsonString, err := json.Marshal(wirelessNetworks)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, "%s", jsonString)
+}
+
+func createPersonalHotspot(w http.ResponseWriter, req *http.Request) {
+
+	var n networkmanager.APNetwork
+
+	decoder := json.NewDecoder(req.Body)
+
+	err := decoder.Decode(&n)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		ResponseMessage.Success = false
+		ResponseMessage.Message = "Failed to create access point"
+		jsonString, _ := json.Marshal(ResponseMessage)
+		fmt.Fprintf(w, "%s", jsonString)
+		return
+	}
+
+	wirelessDevices, _ := networkmanager.GetDeviceByType(2)
+	wirelessDevice := wirelessDevices[0].(map[string]interface{})["DevicePath"].(dbus.ObjectPath)
+
+	wirelessNetwork := networkmanager.APNetwork{
+		Ssid:     n.Ssid,
+		Password: n.Password,
+		Device:   wirelessDevice,
+	}
+
+	networkmanager.CreateAccessPoint(wirelessNetwork)
+	w.Header().Set("Content-Type", "application/json")
+	ResponseMessage.Success = true
+	ResponseMessage.Message = "Successfully created access point"
+	jsonString, err := json.Marshal(ResponseMessage)
+	fmt.Fprintf(w, "%s", jsonString)
+}
+
+func main() {
+
+	// internetSharing()
+
+	//////////////////////////////
 
 	// Get the list of wireless devices
 	// devicess, _ := networkmanager.GetDeviceByType(2)
@@ -198,12 +245,21 @@ func main() {
 	// fmt.Println("WIRED", wired)
 
 	http.HandleFunc("/settings", settings)
-	http.HandleFunc("/connectivity", connectivity)
-	http.HandleFunc("/devices", devices)
+
+	// Connectivity
+	http.HandleFunc("/connectivity", getConnectivity)
+
+	// Device-related
+	http.HandleFunc("/devices", getAllDevices)
 	http.HandleFunc("/devices/wired", getWiredDevices)
 	http.HandleFunc("/devices/wireless", getWirelessDevices)
 
+	// Network-related
 	http.HandleFunc("/add", addNetwork)
+	http.HandleFunc("/create", createPersonalHotspot)
+	http.HandleFunc("/scan", scanWirelessNetwork)
+
+	// Start the server on port 8888
 
 	http.ListenAndServe(":8888", nil)
 
